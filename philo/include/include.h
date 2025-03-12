68be3c767766d8bc4b32cd1ba822c39583eb07bd
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2025-03-12 12:51:53 +0100

    Archivos .h primarios creados

---
+++ philo/include/include.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 11:12:19 by descamil          #+#    #+#             */
+/*   Updated: 2025/03/12 12:30:33 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_H
+# define INCLUDE_H
+
+# include <stdio.h>
+
+# include "struct.h"
+# include "main.h"
+# include "execution.h"
+
+#endif
