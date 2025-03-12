75807ed6f4120de85d661f73968648cd2615485f
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2025-03-12 16:08:55 +0100

    Ft_calloc.c

---
+++ philo/src/utils/ft_calloc.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 15:40:04 by descamil          #+#    #+#             */
+/*   Updated: 2025/03/12 15:40:24 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/utils.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	i;
+	size_t	total_size;
+	char	*ptr_chr;
+
+	if ((nmemb == SIZE_MAX) || (size == SIZE_MAX))
+		return (NULL);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	ptr_chr = (char *)ptr;
+	while (i < total_size)
+	{
+		ptr_chr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
