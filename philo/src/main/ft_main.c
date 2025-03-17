4b9feab69d0dc1ab0dae4067823ed7b111226e46
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2025-03-17 13:32:42 +0100

    Nueva estructura de tiempos y funciones de liberación, Creacion de hilos

--- philo/src/main/ft_main.c
+++ philo/src/main/ft_main.c
@@ -6,56 +6,107 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 11:08:52 by descamil          #+#    #+#             */
-/*   Updated: 2025/03/12 16:06:54 by descamil         ###   ########.fr       */
+/*   Updated: 2025/03/17 13:27:19 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/main.h"
 
-static t_philo *ft_manage_atoi_error(int error)
+void	ft_free_philo(t_philo *philo)
+{
+	int	i;
+
+	if (!philo)
+		return;
+	i = 0;
+	while (i < philo[0].time.number_philos)
+	{
+		pthread_mutex_destroy(&philo[i].meal_mutex);
+		i++;
+	}
+	free(philo);
+}
+
+void	ft_free_time(t_times *time)
+{
+	if (time)
+		free(time);
+}
+
+long	get_timestamp()
+{
+	struct timeval	tv;
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+static t_philo *ft_create_pthreads(t_times *time)
+{
+	t_philo	*philos;
+	int		i;
+
+	philos = ft_calloc(sizeof(t_philo), time->number_philos);
+	if (!philos)
+		return (NULL);
+	i = 0;
+	while (i < time->number_philos)
+	{
+		philos[i].id = i + 1;
+		philos[i].last_meal_time = get_timestamp();
+		philos[i].time = *time;
+		pthread_mutex_init(&philos[i].meal_mutex, NULL);
+		i++;
+	}
+	return (philos);
+}
+
+static t_times *ft_manage_atoi_error(int error)
 {
 	if (error == 1)
-		printf("Invalid format: input is not a valid number.\n");
+		printf(B_RD_1"Invalid format: input is not a valid number.\n"RESET);
 	if (error == 2)
-		printf("Overflow: number exceeds INT_MAX.\n");
+		printf(B_RD_1"Overflow: number exceeds INT_MAX.\n"RESET);
 	if (error == 3)
-		printf("Underflow: number is smaller than INT_MIN.\n");
+		printf(B_RD_1"Invalid number: Number cannot be negative.\n"RESET);
 	if (error == 4)
-		printf("Syntax error: invalid characters after the number.\n");
+		printf(B_RD_1"Syntax error: invalid characters.\n"RESET);
+	if (error != 0)
+		printf(B_GR_1"\nExample: ./philo 5 800 500 200 [6]\n\n"RESET);
 	return (NULL);
 }
+
 // number_of_philosophers
 // time_to_die
 // time_to_eat
 // time_to_sleep
 // [number_of_times_each_philosopher_must_eat]
-static t_philo	*ft_create_struct(char **argv, int argc)
+static t_times	*ft_create_time(char **argv, int argc)
 {
-	t_philo	*philo;
+	t_times	*time;
 	int		error;
 
-	philo = ft_calloc(sizeof(t_philo), 1);
-	if (philo == NULL)
-		return (NULL);		
-	philo->time.number_philos = ft_atoi_mod(argv[1], &error);
+	time = ft_calloc(sizeof(t_times), 1);
+	if (time == NULL)
+		return (NULL);
+	time->number_philos = ft_atoi_mod(argv[1], &error);
 	if (error > 0 && error < 5)
 		return(ft_manage_atoi_error(error));
-	philo->time.time_to_die = ft_atoi_mod(argv[2], &error);
+	time->time_to_die = ft_atoi_mod(argv[2], &error);
 	if (error > 0 && error < 5)
 		return(ft_manage_atoi_error(error));
-	philo->time.time_to_eat = ft_atoi_mod(argv[3], &error);
+	time->time_to_eat = ft_atoi_mod(argv[3], &error);
 	if (error > 0 && error < 5)
 		return(ft_manage_atoi_error(error));
-	philo->time.time_to_sleep = ft_atoi_mod(argv[4], &error);
+	time->time_to_sleep = ft_atoi_mod(argv[4], &error);
 	if (error >= 1 && error <= 4)
 		return(ft_manage_atoi_error(error));
-	philo->time.eat_limit = -1;
+	time->eat_limit = -1;
 	if (argc != 6)
-		return (philo);
-	philo->time.eat_limit = ft_atoi_mod(argv[5], &error);
-	if (error > 0 && error < 5)
+		return (time);
+	time->eat_limit = ft_atoi_mod(argv[5], &error);
+	if (error > 0 && error < 6)
 		return(ft_manage_atoi_error(error));
-	return (philo);
+	return (time);
 }
 
 static int	ft_return_printf(const char *str)
@@ -67,15 +118,18 @@ static int	ft_return_printf(const char *str)
 int main(int argc, char **argv)
 {
 	t_philo	*philo;
+	t_times	*time;
 
 	if (argc < 5)
 		return(ft_return_printf("Few arguments"));
 	if (argc > 6)
 		return(ft_return_printf("Too many arguments"));
-	philo = ft_create_struct(argv, argc);
-	if (philo == NULL)
+	time = ft_create_time(argv, argc);
+	if (time == NULL)
 		return (1);
-	ft_print_philo(philo);
-	free(philo);
+	print_times(time);
+	philo = ft_create_pthreads(time);
+	ft_free_philo(philo);
+	ft_free_time(time);
 	return (0);
 }
