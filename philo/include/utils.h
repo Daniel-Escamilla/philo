c5506b161afb747024247e6225b682123a5ef15a
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2025-03-17 13:29:16 +0100

    Declaracion de las funciones de utils.h

--- philo/include/utils.h
+++ philo/include/utils.h
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 15:23:06 by descamil          #+#    #+#             */
-/*   Updated: 2025/03/12 15:51:51 by descamil         ###   ########.fr       */
+/*   Updated: 2025/03/17 13:18:16 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,21 @@
 
 # include "../philo.h"
 
+// ft_atoi_mod.c
 int		ft_atoi_mod(char *str, int *error);
-void	ft_print_philo(t_philo *philo);
+
+// ft_calloc.c
 void	*ft_calloc(size_t nmemb, size_t size);
+
+// ft_itoa.c
+char	*ft_itoa(long n);
+
+// ft_print_philo.c
+void	print_times(t_times *times);
+
+// ft_utils.c
+int		ft_strlen(const char *str);
+void	ft_exit_perror(char *error, int status);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
